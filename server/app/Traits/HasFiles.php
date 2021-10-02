6d05e7476940012fe044f5c74f62f31babe8fc99
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-10-02 21:50:51 +0800

    Super Mega Update

--- server/app/Traits/HasFiles.php
+++ server/app/Traits/HasFiles.php
@@ -6,20 +6,11 @@ use App\Models\File;
 
 trait HasFiles
 {
-    public $hasFilesName = 'files';
-
     /**
      * @return \Illuminate\Database\Eloquent\Relations\MorphMany
      */
     public function files()
     {
         return $this->morphMany(File::class, 'fileable');
-    }
-
-    public static function bootHasFiles()
-    {
-        static::deleting(function (HasFiles $model) {
-            $model->{$model->hasFilesName}->delete();
-        });
     }
 }
